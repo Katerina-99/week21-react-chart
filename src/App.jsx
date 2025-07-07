326019ba3fc3413efb388580446fe5fdca434352
Katerina-99 <rakitinakaty555@gmail.com>
2025-07-07 17:19:48 +0100

    added chart daily activities

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Chart } from "react-google-charts";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 1],
+    ["Sleep", 7],
+    ["Read", 1],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
     </>
   )
 }
